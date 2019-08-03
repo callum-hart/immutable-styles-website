82c96bc60f6f02cfd199f2204ef43d3e87a4564b
Callum Hart <callum@callumhart.com>
2019-08-03 23:37:51 +0100

    Improve logo styling

--- src/atoms/logo.iss.jsx
+++ src/atoms/logo.iss.jsx
@@ -7,21 +7,15 @@ export default (
     align-items: center;
 
     <object className="logo-image">
-      width: 80px;
+      width: 100px;
     </object>
 
-    <div className="logo-text">
-      <p>
-        font-family: 'IBM Plex Mono';
-        font-size: var(--font-s);
-        color: {palette.mediumContrast};
-      </p>
-      <p pseudo=":first-of-type">
-        margin: 0 0 0 6px;
-      </p>
-      <p pseudo=":last-of-type">
-        margin: 0;
-      </p>
-    </div>
+    <p className="logo-text">
+      max-width: 16ch;
+      text-indent: 8px;
+      font-family: 'IBM Plex Mono';
+      font-size: var(--font-s);
+      color: {palette.mediumContrast};
+    </p>
   </div>
 );
